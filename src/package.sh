b03e7c341c55c2568dfbbc120719473b557f2ab3
GH0STAV0 <you@example.com>
2022-07-07 21:15:44 +0100

    my connecto python

--- src/package.sh
+++ src/package.sh
@@ -33,7 +33,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt update &&  apt install google-chrome-stable -y
+apt update &&  apt install google-chrome-stable ffmpeg -y
 
 
 wget https://chromedriver.storage.googleapis.com/103.0.5060.53/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod +x chromedriver && cp chromedriver /usr/bin/
